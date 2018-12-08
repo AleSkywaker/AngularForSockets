51a291e3c75dded710f16e60c9de599853908592
Alex Colombo <uricaine@hotmail.com>
2018-12-08 10:22:15 +0100

    fix server

--- 01-server/classes/server.ts
+++ 01-server/classes/server.ts
@@ -30,7 +30,7 @@ export default class Server {
     this.io.on("connection", cliente => {
       console.log("Cliente conectado");
       //Escuchar mensaje
-      socket.mensaje(cliente);
+      socket.mensaje(cliente, this.io);
 
       //Desconectar
       socket.desconectar(cliente);
