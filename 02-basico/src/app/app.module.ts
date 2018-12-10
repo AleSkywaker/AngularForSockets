5420b414beabc3a7b4c020bb06e79528234eb8da
Alex Colombo <uricaine@hotmail.com>
2018-12-10 16:08:19 +0100

    update app.component y app.module

--- 02-basico/src/app/app.module.ts
+++ 02-basico/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
 
@@ -10,10 +11,25 @@ import { environment } from "src/environments/environment";
 import { AppComponent } from "./app.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { ChatComponent } from "./components/chat/chat.component";
+import { ListaUsuariosComponent } from "./components/lista-usuarios/lista-usuarios.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { MensajesComponent } from "./pages/mensajes/mensajes.component";
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, ChatComponent],
-  imports: [BrowserModule, FormsModule, SocketIoModule.forRoot(config)],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
