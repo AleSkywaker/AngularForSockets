0e9fa107d453b5df2999562c98f182f92bc0e867
Alex Colombo <uricaine@hotmail.com>
2018-12-10 16:09:17 +0100

    update webSocketsService

--- 02-basico/src/app/services/websocket.service.ts
+++ 02-basico/src/app/services/websocket.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from "@angular/core";
 import { Socket } from "ngx-socket-io";
+import { Usuario } from "../classes/usuario";
 
 @Injectable({
   providedIn: "root"
 })
 export class WebsocketService {
   public socketStatus = false;
+  public usuario: Usuario;
 
   constructor(private socket: Socket) {
+    this.cargarStorage();
     this.checkStatus();
+  }
+  getUsuario() {
+    return this.usuario;
   }
 
   checkStatus() {
@@ -29,5 +35,24 @@ export class WebsocketService {
 
   listen(evento: string) {
     return this.socket.fromEvent(evento);
+  }
+
+  loginWs(nombre: string) {
+    return new Promise((res, rej) => {
+      this.emit("configurar-usuario", { nombre }, resp => {
+        this.usuario = new Usuario(nombre);
+        this.guardaStoreage();
+        res();
+      });
+    });
+  }
+  guardaStoreage() {
+    localStorage.setItem("usuario", JSON.stringify(this.usuario));
+  }
+
+  cargarStorage() {
+    if (localStorage.getItem("usuario")) {
+      this.usuario = JSON.parse(localStorage.getItem("usuario"));
+    }
   }
 }
