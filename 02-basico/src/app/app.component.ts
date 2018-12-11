a5d8954d40eb876501cee3ccedde8bb3281b7a2c
Alex Colombo <uricaine@hotmail.com>
2018-12-11 20:40:21 +0100

    create privateMessages method

--- 02-basico/src/app/app.component.ts
+++ 02-basico/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { WebsocketService } from "./services/websocket.service";
+import { ChatService } from "./services/chat.service";
 
 @Component({
   selector: "app-root",
@@ -9,9 +10,16 @@ import { WebsocketService } from "./services/websocket.service";
 export class AppComponent implements OnInit {
   private title: string;
 
-  constructor(public wsService: WebsocketService) {
+  constructor(
+    public wsService: WebsocketService,
+    public chatService: ChatService
+  ) {
     this.title = "App-Socket";
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.chatService.getMessagesPrivate().subscribe(msg => {
+      console.log("privado", msg);
+    });
+  }
 }
