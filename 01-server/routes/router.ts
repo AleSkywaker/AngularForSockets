97baf26e903a1c3f47ee70b34fd515bfd0532d44
Alex Colombo <uricaine@hotmail.com>
2018-12-12 22:59:15 +0100

    new payload

--- 01-server/routes/router.ts
+++ 01-server/routes/router.ts
@@ -3,7 +3,16 @@ import Server from "../classes/server";
 
 const router = Router();
 
-router.get("/mensajes", (req: Request, res: Response) => {
+router.post("/mensajes", (req: Request, res: Response) => {
+  const cuerpo = req.body.cuerpo;
+  const de = req.body.de;
+
+  const payload = { cuerpo, de };
+
+  const server = Server.instance;
+
+  server.io.emit("mensaje-nuevo", payload);
+
   res.json({
     ok: true,
     mensaje: "Todo esta bien!!!"
@@ -28,4 +37,22 @@ router.post("/mensajes/:id", (req: Request, res: Response) => {
     mensaje: `POST - LISTO ${cuerpo} ${de} ${id} NUEVO`
   });
 });
+
+router.get("/usuarios", (req: Request, res: Response) => {
+  const server = Server.instance;
+
+  server.io.clients((err: any, clientes: string[]) => {
+    if (err) {
+      return res.json({
+        ok: false,
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      clientes
+    });
+  });
+});
+
 export default router;
