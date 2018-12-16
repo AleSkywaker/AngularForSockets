42df3a232a8ec39bc107d9a1dc7ab14a8de121e5
Alex Colombo <uricaine@hotmail.com>
2018-12-16 18:34:20 +0100

    inject service in lista usuarios

--- 02-basico/src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ 02-basico/src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ChatService } from "src/app/services/chat.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-lista-usuarios',
-  templateUrl: './lista-usuarios.component.html',
-  styleUrls: ['./lista-usuarios.component.css']
+  selector: "app-lista-usuarios",
+  templateUrl: "./lista-usuarios.component.html",
+  styleUrls: ["./lista-usuarios.component.css"]
 })
 export class ListaUsuariosComponent implements OnInit {
+  public usuariosActivos: Observable<any>;
 
-  constructor() { }
+  constructor(public chatService: ChatService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
